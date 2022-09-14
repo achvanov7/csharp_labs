6c0752bc6e1ffb01542dd39a32c183b026f5eb2f
Alexander Chvanov <58791253+achvanov7@users.noreply.github.com>
2022-09-14 19:31:45 +0300

    MinStack and Tests

---
+++ HW1/MinStack/MinStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinStack
+{
+    public class MinStack<T> where T : IComparable<T>
+    {
+        private readonly Stack<T> _stack;
+        private readonly Stack<T> _mins;
+        private int _size;
+
+        private void CheckEmpty()
+        {
+            if (_size == 0)
+            {
+                throw new Exception("Stack is empty");
+            }
+        }
+
+        public MinStack()
+        {
+            _stack = new Stack<T>();
+            _mins = new Stack<T>();
+            _size = 0;
+        }
+
+        public void Push(T elem)
+        {
+            if (_size == 0 || elem.CompareTo(_mins.Peek()) <= 0)
+            {
+                _mins.Push(elem);
+            }
+            else
+            {
+                _mins.Push(_mins.Peek());
+            }
+
+            _stack.Push(elem);
+            _size += 1;
+        }
+
+        public T Pop()
+        {   
+            CheckEmpty();
+            _mins.Pop();
+            _size -= 1;
+            return _stack.Pop();
+        }
+
+        public T Min()
+        {   
+            CheckEmpty();
+            return _mins.Peek();
+        }
+
+        public T Peek()
+        {
+            CheckEmpty();
+            return _stack.Peek();
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+    }
+}
---
+++ HW1/Tests/Tests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MinStack;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MinStackTests
+    {
+        [Test]
+        public void PushAndPeekTest()
+        {
+            var minStack = new MinStack<int>();
+            const int N = 1000;
+            for (var i = 0; i < N; ++i)
+            {
+                minStack.Push(i);
+                Assert.IsTrue(i == minStack.Peek());
+            }
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            var minStack = new MinStack<int>();
+            const int N = 1000;
+            for (var i = 0; i < N; ++i)
+            {
+                minStack.Push(i);
+            }
+            for (var i = N - 1; i >= 0; --i)
+            {
+                Assert.IsTrue(i == minStack.Pop());
+            }
+        }
+
+        [Test]
+        public void SizeTest()
+        {
+            var minStack = new MinStack<int>();
+            const int N = 1000;
+            for (var i = 0; i < N; ++i)
+            {
+                minStack.Push(i);
+                Assert.IsTrue(i + 1 == minStack.Size());
+            }
+            for (var i = N - 1; i >= 0; --i)
+            {
+                minStack.Pop();
+                Assert.IsTrue(i == minStack.Size()); 
+            }
+        }
+
+        [Test]
+        public void MinTest()
+        {
+            var minStack = new MinStack<int>();
+            const int N = 1000;
+            var stack = new Stack<int>();
+            var rng = new Random();
+            for (var i = 0; i < N; ++i)
+            {
+                var r = rng.Next(0, 1);
+                if (minStack.Size() == 0 || r == 0)
+                {
+                    var elem = rng.Next();
+                    minStack.Push(elem);
+                    stack.Push(elem);
+                }
+                else
+                {
+                    Assert.IsTrue(minStack.Size() > 0);
+                    minStack.Pop();
+                    stack.Pop();
+                }
+
+                if (minStack.Size() > 0)
+                {
+                    Assert.IsTrue(minStack.Min() == stack.Min());
+                }
+            }
+        }
+    }
+}
