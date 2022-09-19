723f109f3a28630f47349abb2fdf96fc5b6ea209
Alexander Chvanov <58791253+achvanov7@users.noreply.github.com>
2022-09-19 23:17:42 +0300

    add HW2 and DiceRollSolver(Task 4)

---
+++ HW2/DiceRollSolveTool/DiceRollSolver.cs
@@ -0,0 +1,42 @@
+﻿namespace DiceRollSolveTool
+{
+    public class DiceRollSolver
+    {
+        public int Solve(int n, int sum)
+        {
+            if (sum < n || sum > 6 * n)
+            {
+                return 0;
+            }
+            var dp = new int[7, 37];
+            dp[0, 0] = 1;
+            for (var i = 0; i < n; ++i)
+            {
+                for (var j = i; j <= 6 * i; ++j)
+                {
+                    for (var k = 1; k <= 6; ++k)
+                    {
+                        dp[i + 1, j + k] += dp[i, j];
+                    }
+                }
+            }
+            return dp[n, sum];
+        }
+
+        public int NaiveSolve(int n, int sum)
+        {
+            if (n == 0)
+            {
+                return sum == 0 ? 1 : 0;
+            }
+
+            var res = 0;
+            for (var i = 1; i <= 6; ++i)
+            {
+                res += NaiveSolve(n - 1, sum - i);
+            }
+
+            return res;
+        }
+    }
+}
