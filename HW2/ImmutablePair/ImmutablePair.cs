2eef2ca45b05aaa403ea719664d5e00ff41dbac2
Alexander Chvanov <58791253+achvanov7@users.noreply.github.com>
2022-09-21 00:38:15 +0300

    add ImmutableType(Task 2)

---
+++ HW2/ImmutablePair/ImmutablePair.cs
@@ -0,0 +1,39 @@
+﻿namespace ImmutablePair
+{
+    public class ImmutablePair<T1, T2>
+    {
+        private readonly T1 _first;
+        private readonly T2 _second;
+
+        public ImmutablePair(T1 x, T2 y)
+        {
+            _first = x;
+            _second = y;
+        }
+
+        public ImmutablePair<T1, T2> ChangeFirst(T1 nfirst)
+        {
+            return new ImmutablePair<T1, T2>(nfirst, _second);
+        }
+
+        public ImmutablePair<T1, T2> ChangeSecond(T2 nsecond)
+        {
+            return new ImmutablePair<T1, T2>(_first, nsecond);
+        }
+
+        public T1 GetFirst()
+        {
+            return _first;
+        }
+
+        public T2 GetSecond()
+        {
+            return _second;
+        }
+
+        public override string ToString()
+        {
+            return $"{_first}, {_second}";
+        }
+    }
+}
---
+++ HW2/Tests/ImmutableTypeTests/ImmutableTypeTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ImmutablePair;
+
+namespace Tests.ImmutableTypeTests
+{   
+    [TestFixture]
+    public class ImmutableTypeTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var pair = new ImmutablePair<int, string>(42, "abcabca");
+            Assert.True(pair.GetFirst() == 42);
+            Assert.True(pair.GetSecond() == "abcabca");
+        }
+
+        [Test]
+        public void ChangeTest()
+        {
+            var pair = new ImmutablePair<int, string>(42, "abcabca");
+            pair = pair.ChangeFirst(43);
+            Assert.True(pair.GetFirst() == 43);
+            pair = pair.ChangeSecond("abc");
+            Assert.True(pair.GetSecond() == "abc");
+        }
+
+        [Test]
+        public void ImmutabilityTest()
+        {
+            var pair = new ImmutablePair<int, string>(42, "abcabca");
+            pair.ChangeFirst(43);
+            Assert.True(pair.GetFirst() == 42);
+            pair.ChangeSecond("abc");
+            Assert.True(pair.GetSecond() == "abcabca");
+        }
+    }
+}
